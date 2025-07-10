969741e9aeb4e145fe72d3088779806eeb14cb00
kksh001 <kamakoma@gmail.com>
2025-07-10 21:07:57 +0800

    Add test files

---
+++ RecordingTranscriptionAppTests/RecordingTranscriptionAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecordingTranscriptionAppTests.swift
+//  RecordingTranscriptionAppTests
+//
+//  Created by kamakoma wu on 2025/5/11.
+//
+
+import Testing
+
+struct RecordingTranscriptionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
